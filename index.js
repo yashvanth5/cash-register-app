78bee5da282b19a2ad69c235fef76bea723f4f5a
yashvanth5 <116138513+yashvanth5@users.noreply.github.com>
2022-12-05 20:16:57 +0530

    done
    all input output and processing is done

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const billAmount = document.querySelector("#input-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".numb-notes");
 
-console.log(cashGiven.value);
-
-
-
+const availableNotes = [2000,500,100,20,10,5,1];
 
 checkButton.addEventListener ("click" , function validateBillAndCashAmount(){
-  message.style.display = "none";
-
- if (billAmount.value > 0) {
-  if (cashGiven.value ≥ billAmount.value ){
+  hideMessage();
+  if (billAmount.value > 0) {
+  if (cashGiven.value >= billAmount.value ){
+    const amountToBeReturned = cashGiven.value - billAmount.value;
+    calculateChange(amountToBeReturned );
 
   } else {
-    showMessage("the cash provided should be alteast equal to bill amount")
+    showMessage ("the cash provided should be alteast equal to bill amount")
     
   }
 
@@ -27,7 +26,28 @@ checkButton.addEventListener ("click" , function validateBillAndCashAmount(){
 
 } );
 
-function showMessage(message){
-  message.style.display = "block";
-    message.innerText = message;
+function calculateChange (amountToBeReturned){
+  for(let i = 0 ; i < availableNotes.length ; i++ ) {
+ const numberOfNotes = Math.trunc
+ ( amountToBeReturned / availableNotes[i] 
+ );
+  amountToBeReturned %= availableNotes[i];
+  noOfNotes[i].innerText = numberOfNotes;
+  
+
+  }
+  
+ 
 }
+function hideMessage(){
+
+  message.style.display = "none";
+}
+
+function showMessage(msg){
+  message.style.display = "block";
+    message.innerText = msg;
+}
+
+// made this mistake :
+// I changed the selector of noOfNotes to 'document.querySelectorAll(".numb-notes")' to select all elements with the numb-notes class instead of just one.
